5a39979fe78b7ef42cdef7ae010e88cc3b544701
nitin-mahour <nitin.mahour6@gmail.com>
2020-11-28 19:11:05 +0530

    improved api security and added admin functionality

---
+++ api/admin/list-users.php
@@ -0,0 +1,44 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+require_once('../db.php');
+
+$db = new Database();
+$db = $db->getConnection();
+
+$key = isset($_GET['key']) ? $_GET['key'] : null;
+$sql = "";
+
+if (empty($key)) {
+
+    print json_encode("KEY_MISSING");
+
+} else {
+
+    if ($key == "ncoder") {
+        
+        $sql = "SELECT `id`,`username` FROM `users`";
+        
+        $msgs = $db->query($sql);
+        $res = array();
+        
+        while ($msg = $msgs->fetch_object()) {
+            $res[] = $msg;
+        }
+        
+        if ($res == []) {
+            print json_encode("EMPTY");
+        }
+        else {
+            print json_encode($res);
+        }
+
+    } else {
+
+        print json_encode("KEY_ERROR");
+    }
+
+}
+
+?>
